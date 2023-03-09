174274d8401401859cd2ba31632cacb31fa9ee82
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-03-09 11:34:52 -0300

    Ejercicio 04 funciones realizado y readmi completado

--- js/funciones/ejercicio04.js
+++ js/funciones/ejercicio04.js
@@ -2,3 +2,15 @@
  Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, 
  la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
  */
+
+ let numero = parseInt(prompt("Ingrese el número del que desea ver la tabla de multiplicar: "));
+
+function tablaMultiplicar(numero) {
+  for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    document.write(`${numero} x ${i} = ${resultado}`);
+    document.write("<br>")
+
+  }
+}
+tablaMultiplicar(numero);
